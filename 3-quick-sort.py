d2606f2e4e15df05cd571c41b01dd3b7a5467941
KevDev0247 <donkevin1980s@gmail.com>
2020-12-19 13:35:36 -0500

    Add rightmost pivot choice

--- 3-quick-sort.py
+++ 3-quick-sort.py
@@ -1,12 +1,20 @@
-def partition(array, left, right):
-    pivot = array[left]
-    i = left
+def partition(array, choice, left, right):
+    global count, i
+
+    if choice == 1:
+        pivot = array[left]
+        i = left
+
+    elif choice == 2:
+        pivot = array[right]
+        i = right
+
     array[left], array[i] = array[i], array[left]
 
     i = left + 1
     for j in range(i, right + 1):
+        count += 1
         if array[j] < pivot:
-
             array[i], array[j] = array[j], array[i]
             i += 1
 
@@ -16,14 +24,14 @@ def partition(array, left, right):
     return i
 
 
-def count_quick_sort(array, left, right):
-    if len(array) == 1:
-        return array
+def count_quick_sort(array, choice, left, right):
+    if len(array) < 1:
+        return
     if left <= right:
-        pivot = partition(array, left, right)
+        pivot = partition(array, choice, left, right)
 
-        count_quick_sort(array, left, pivot - 1)
-        count_quick_sort(array, pivot + 1, right)
+        count_quick_sort(array, choice, left, pivot - 1)
+        count_quick_sort(array, choice, pivot + 1, right)
 
 
 file = open("D:\\Algorithms-Specialization\\files\\quick_sort_integers.txt")
@@ -35,6 +43,9 @@ for n in range(0, len(lines)):
     lines[n] = lines[n].strip()
     num_array.append(int(lines[n]))
 
-count_quick_sort(num_array, 0, len(num_array) - 1)
+choice = int(input("input your choice of pivot (1: Leftmost, 2: Rightmost): "))
+count = 0
+count_quick_sort(num_array, choice, 0, len(num_array) - 1)
 
 print(num_array)
+print(count)
