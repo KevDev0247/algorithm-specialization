47932200eb28dbf04cf012c1226bb794033d362b
KevDev0247 <donkevin1980s@gmail.com>
2021-01-11 23:22:29 -0500

    Submit assignment 4

--- 4-karger-algorithm-count-min-cuts.py
+++ 4-karger-algorithm-count-min-cuts.py
@@ -1,7 +1,54 @@
-import re
-import random
+import random, copy
+
 
 def load_graph():
-    g = {}
+    file = open("D:\\Algorithms-Specialization\\files\\min-cut-graph-integers.txt")
+    line = file.read().strip().split("\n")
 
-    f = open("D:\\Algorithms-Specialization\\files\\quick_sort_integers.txt")
+    graph_dict = {}
+    for element in line:
+        line_list = list(map(int, element.strip().split("\t")))
+        graph_dict[line_list[0]] = line_list[1:]
+
+    return graph_dict
+
+
+def get_random_edge(graph_dict):
+    a = random.choice(list(graph_dict.keys()))
+    b = random.choice(graph_dict[a])
+
+    edge = (a, b)
+    return edge
+
+
+def karger_algorithm(graph_dict):
+    num = []
+
+    while len(graph_dict) > 2:
+        a, b = get_random_edge(graph_dict)
+        graph_dict[a].extend(graph_dict[b])
+
+        for c in graph_dict[b]:
+            graph_dict[c].remove(b)
+            graph_dict[c].append(a)
+
+        while a in graph_dict[a]:
+            graph_dict[a].remove(a)
+
+        del graph_dict[b]
+
+    for key in graph_dict:
+        num.append(len(graph_dict[key]))
+
+    return num[0]
+
+
+graph = load_graph()
+min_cut = 1000
+for i in range(100):
+    graph = copy.deepcopy(graph)
+    cut = karger_algorithm(graph)
+    if cut < min_cut:
+        min_cut = cut
+
+print(min_cut)
